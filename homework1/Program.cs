45f88a5a8cb7598e891f26f5c4630431cbd9c143
Tatyana <yerm.tat@gmail.com>
2023-08-23 12:38:13 +0400

    Add project files.

---
+++ homework1/Program.cs
@@ -0,0 +1,33 @@
+﻿
+//Задание 1
+//Выведите на экран цитату Бьярна Страуструпа используя escape sequences (\n,\t,\b, e.t.c):
+//It 's easy to win forgiveness for being wrong; being right is what gets you into real trouble. Bjarne Stroustrup
+
+
+//Console.WriteLine("It 's easy to win forgiveness for being wrong;\n\tbeing right is what gets you into real trouble." +
+//    "\n\t\t\t\t\tBjarne Stroustrup");
+
+
+
+//Задание 2
+//Есть два числа. Первое число — это значение, второе число процент, который необходимо посчитать. Например, 90 и 10. 
+//Требуется вывести на экран 10 процентов от 90. 
+//Результат: 9.
+
+
+//int number = 90;
+//int percent = 10;
+
+//Console.WriteLine(percent * number / 100);
+
+
+
+//Задание 3
+//Есть четыре цифры. Необходимо создать число, содержащее эти цифры. Например, если цифры 1, 5, 7, 8 тогда нужно сформировать число 1578.
+//Примечание: Надо сформировать число а не вывести на экран в такой поледовательности 4 цифр.
+
+
+//int numb1 = 1, numb2 = 5, numb3 = 7, numb4 = 8;
+
+//int number = numb1 * 1000 + numb2 * 100 + numb3 * 10 + numb4;
+//Console.WriteLine(number);
